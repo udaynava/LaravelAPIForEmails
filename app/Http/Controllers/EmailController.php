bc604fd821c386e551db9c9f5b2353735f50dc1f
Uday Krishnan <uday@Udays-MacBook-Air.local>
2021-12-21 15:53:59 -0500

    Remove Logs

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -11,28 +11,13 @@ class EmailController extends Controller
     public function sendEmail(Request $request) {
 
         if ($request->isMethod('post')) {
-            // $userData = $request->input('emails');
-            // Log::info("userdate value down");
-            // Log::info($userData);
-            // echo "<prev>"; print_r($userData); die;
 
             $emailData = $request->input('emails');
-            // Log::info("Incoming input value down");
-            // Log::info($emailData);
-            // echo "<prev>"; print_r($comingData); die;
 
             foreach($emailData as $email) {
-                // Log::info("Each email value down");
-                // Log::info($email);
-                // Mail::to($email['email'])->subject($email['subject']);
                 dispatch(new MailJob($email));
             }
         }
-
-        // return ["Success req"];
-        
-        // dispatch(new MailJob());
-
-        // dd('Email has been delivered');
+        return ["Success req"];
     }
 }
